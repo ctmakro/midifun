484a3c9ded8569c86cae67110d82be92c75f415e
ctmakro <ctmakro@gmail.com>
2017-12-07 17:57:45 +0800

    update iter.py

--- iter.py
+++ iter.py
@@ -1,22 +1,22 @@
-import os
+import os,time
+from midiutil import mido, outport, open_midifile
 
 midipath = './midies/'
 names = os.listdir(midipath)
 
 midies = list(filter(lambda n:True if n.lower().endswith('.mid') else False, names))
 
-# print(midies)
-
-for fn in midies:
-    fn = midipath+fn
-    print(fn)
-
-    from mido import MidiFile
-    midifile = MidiFile(fn)
-
+def show_info(midifile):
     print('n of tracks',len(midifile.tracks))
     print('type',midifile.type)
     print('tpb',midifile.ticks_per_beat)
-    print([len(t) for t in midifile.tracks])
+    print('length of each track', [len(t) for t in midifile.tracks])
 
-    del midifile
+def show_all_info():
+    for fn in midies:
+        midifile = open_midifile(midipath+fn)
+        show_info(midifile)
+        del midifile
+
+if __name__ == '__main__':
+    show_all_info()
